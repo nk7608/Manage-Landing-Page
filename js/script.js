eaedc481a504ca182ab21fb052ff654377508cc9
Nayana Singh <nayanak872@gmail.com>
2022-11-15 15:45:06 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
 
 btn.addEventListener('click', () => {
      btn.classList.toggle('open')
      nav.classList.toggle('flex')
      nav.classList.toggle('hidden')
+})
+
+form.addEventListener('submit', (e) =>{
+     e.preventDefault();
+     let messages = []
+     if (email.value === '' || email.value == null) {
+       messages.push('Name is required')
+     }
+     if (messages.length > 0) {
+          e.preventDefault()
+          errorElement.innerText = messages.join(', ')
+     }
 })
